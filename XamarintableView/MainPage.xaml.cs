46dcab681d1a2fb1ea5604e2dfd1a4c5cb18e2bd
Daniel <daniel.hjartstrom@gmail.com>
2020-01-23 15:49:32 +0100

    Added a ICommand

--- XamarintableView/MainPage.xaml.cs
+++ XamarintableView/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace XamarintableView
@@ -11,11 +12,23 @@ namespace XamarintableView
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
+
+        public ICommand ButtonCommand { get; private set; }
+        public string ButtonCommandResult { get; private set; }
+
         public MainPage()
         {
             InitializeComponent();
+            ButtonCommand = new Command<string>(CommandCallMethod);
+            BindingContext = this;
         }
+
+        private void CommandCallMethod(string value) {
+            ButtonCommandResult = value;
+            OnPropertyChanged("ButtonCommandResult");
+        }
+
     }
 }
